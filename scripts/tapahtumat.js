30f5f6ca5690e10c41aca3cd7c690e34c71125d9
devaajanne <devaajanne@gmail.com>
2025-02-28 16:29:24 +0200

    Refactored tapahtumat page and scripts

--- scripts/tapahtumat.js
+++ scripts/tapahtumat.js
@@ -1,16 +1,21 @@
 function naytaTapahtumaData(data) {
-  var teksti = "<h1>Tampereella tapahtuu</h1>";
-  for (var i = 0; i < data.length; i++) {
+  // Etsitään data JSON-tiedostosta
+  let teksti = "<h1 class='text-center'>Tampereella tapahtuu</h1>";
+  for (let i = 0; i < data.length; i++) {
     teksti += `<h3>${data[i].title}</h3>`;
     teksti += `<p>${data[i].description}</p>`;
     teksti += `<p><a href=${data[i].url}>${data[i].url}</a></p>`;
   }
+  // Esitetään data oikeassa kohtaa sivua
   document.getElementById("tapahtumat").innerHTML = teksti;
 }
 
 function haeTapahtumaData() {
+  // Aluksi luodaan URL josta pyydetään GET-pyynnöllä data
+  const URL = "https://api.visittampere.com/api/v1/visittampere/event/published/all/?format=json&lang=fi";
+
   // Haetaan JSON-tietue osoitteesta GET-http-pyynnöllä
-  fetch("https://api.visittampere.com/api/v1/visittampere/event/published/all/?for mat=json&lang=fi")
+  fetch(URL)
     // Parseroi JSON-objektin response bodysta
     .then(function (response) {
       return response.json();
@@ -21,6 +26,6 @@ function haeTapahtumaData() {
     })
     // Jos tapahtuu virhe, näytetään virheilmoitus
     .catch(function (error) {
-      document.getElementById("virhe").innerHTML = "<p>Tietoa ei pystytä hakemaan</p>";
+      document.getElementById("tapahtumatVirhe").innerHTML = "<h1 class='text-center'>Tietoa ei pystytä hakemaan</h1>";
     });
 }
