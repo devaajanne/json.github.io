6d0b53aa4139b750306b705a4b01e3bd9124f786
devaajanne <devaajanne@gmail.com>
2025-02-28 16:27:25 +0200

    Refactored kurssi page and scripts

--- scripts/kurssi.js
+++ scripts/kurssi.js
@@ -3,28 +3,44 @@ function naytaKurssiData(data) {
   document.getElementById("kuvaus").textContent = data.kuvaus;
   document.getElementById("kuva").src = data.kuva;
 
-  let opintojaksonTiedot = "<h2>Opintojakson tiedot:</h2><ul>";
+  let opintojaksonTiedot = "<h2>Opintojakson tiedot:</h2>";
+  opintojaksonTiedot += "<ul>";
   opintojaksonTiedot += `<li>Nimi: ${data.opintojakso.nimi}</li>`;
   opintojaksonTiedot += `<li>Tunnus: ${data.opintojakso.tunnus}</li>`;
-  opintojaksonTiedot += `<li>Tunnus: ${data.opintojakso.opintopisteet}</li></ul>`;
-  document.getElementById("opintojakso").innerHTML = opintojaksonTiedot;
+  opintojaksonTiedot += `<li>Tunnus: ${data.opintojakso.opintopisteet}</li>`;
+  opintojaksonTiedot += "</ul>";
+  opintojaksonTiedot += document.getElementById("opintojakso").innerHTML = opintojaksonTiedot;
 
-  let sisaltoJaTekniikatHTML = "<h2>Tekniikat</h2><table class='table table-hover'>";
-  sisaltoJaTekniikatHTML += "<tr><th>Viikko</th><th>Aihe</th><th>Tekniikka</th><th>Linkki</th></tr>";
+  // Alustetaan taulukko ja iteroidaan data siihen JSON-tiedostosta
+  let sisaltoJaTekniikatHTML = "<h2>Tekniikat</h2>";
+  sisaltoJaTekniikatHTML += "<table class='table table-hover'>";
+  sisaltoJaTekniikatHTML += "<tr>";
+  sisaltoJaTekniikatHTML += "<th>Viikko</th>";
+  sisaltoJaTekniikatHTML += "<th>Aihe</th>";
+  sisaltoJaTekniikatHTML += "<th>Tekniikka</th>";
+  sisaltoJaTekniikatHTML += "<th>Linkki</th>";
+  sisaltoJaTekniikatHTML += "</tr>";
+
   for (let i = 0; i < Object.keys(data.opintojakso.tekniikat).length; i++) {
     sisaltoJaTekniikatHTML += "<tr>";
     sisaltoJaTekniikatHTML += `<td>${i}</td>`;
     sisaltoJaTekniikatHTML += `<td>${data.opintojakso.sisalto[i]}</td>`;
     sisaltoJaTekniikatHTML += `<td>${data.opintojakso.tekniikat[i].aihe}</td>`;
-    sisaltoJaTekniikatHTML += `<td><a href=${data.opintojakso.tekniikat[i].linkki}>${data.opintojakso.tekniikat[i].linkki}</a></td></tr>`;
+    sisaltoJaTekniikatHTML += `<td><a href=${data.opintojakso.tekniikat[i].linkki}>${data.opintojakso.tekniikat[i].linkki}</a></td>`;
+    sisaltoJaTekniikatHTML += "</tr>";
   }
   sisaltoJaTekniikatHTML += "</table>";
+
+  // Esitetään data ja taulukko oikeassa kohtaa sivua
   document.getElementById("sisaltojatekniikat").innerHTML = sisaltoJaTekniikatHTML;
 }
 
 function haeKurssiData() {
+  // Aluksi luodaan URL josta pyydetään GET-pyynnöllä data
+  const URL = "https://devaajanne.github.io/json.github.io/json/kurssi.json";
+
   // Haetaan JSON-tietue osoitteesta GET-http-pyynnöllä
-  fetch("https://devaajanne.github.io/json.github.io/json/kurssi.json")
+  fetch(URL)
     // Parseroi JSON-objektin response bodysta
     .then(function (response) {
       return response.json();
@@ -35,6 +51,6 @@ function haeKurssiData() {
     })
     // Jos tapahtuu virhe, näytetään virheilmoitus
     .catch(function (error) {
-      document.getElementById("virhe").innerHTML = "<p>Tietoa ei pystytä hakemaan</p>";
+      document.getElementById("kurssiVirhe").innerHTML = "<h1 class='text-center'>Tietoa ei pystytä hakemaan</h1>";
     });
 }
