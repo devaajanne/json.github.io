75d882be57c9768254dfff3ec4b5900166f4762c
devaajanne <devaajanne@gmail.com>
2025-02-28 16:30:44 +0200

    Refactored saa page and scripts

--- scripts/saa.js
+++ scripts/saa.js
@@ -1,5 +1,8 @@
 function naytaSaaData(data) {
+  // Etsitään OpenWeatherMapista oikea ikoni säädatalle
   let iconURL = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
+
+  // Luodaan sääkortit JSON-datan pohjalta
   let saaTiedot = '<div class="card-body">';
   saaTiedot += `<h2>${data.name}</h2>`;
   saaTiedot += "<ul>";
@@ -10,14 +13,22 @@ function naytaSaaData(data) {
   saaTiedot += `<img src=${iconURL}>`;
   saaTiedot += "</div>";
 
-  // Tässä määritetään json-datan perusteella mihin HTML-elementtiin saadut tiedot sijoitetaan
-  let HTMLid = `${data.name}saa`.toLowerCase(); // helsinkisaa, tamperesaa, oulusaa
+  // Tässä määritetään JSON-datan perusteella mihin HTML-elementtiin saadut tiedot sijoitetaan
+  let HTMLid = `${data.name}saa`.toLowerCase(); // -> helsinkisaa, tamperesaa, oulusaa
 
+  // Esitetään data ja säätiedot oikeissa korteissa
   document.getElementById(HTMLid).innerHTML = saaTiedot;
 }
 
+// Tämä funktio esittää tämän päivän päivämäärän
+function naytaPaiva() {
+  const date = new Date();
+  let paiva = `<h2 class="text-center">Päivän sää ${dayjs(date).format("DD.M.YYYY")}</h2>`;
+  document.getElementById("paiva").innerHTML = paiva;
+}
+
 function haeSaaData(lat, lon) {
-  // Aluksi luodaan URL josta pyydetään GET-pyynnöllä säädata
+  // Aluksi luodaan URL josta pyydetään GET-pyynnöllä data
   const APIKEY = "665ecd56dfc08dbb50feb8b8f5034e28";
   const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIKEY}&units=metric&lang=fi`;
 
@@ -33,6 +44,6 @@ function haeSaaData(lat, lon) {
     })
     // Jos tapahtuu virhe, näytetään virheilmoitus
     .catch(function (error) {
-      document.getElementById("virhe").innerHTML = "<p>Tietoa ei pystytä hakemaan</p>";
+      document.getElementById("saaVirhe").innerHTML = "<h1 class='text-center'>Tietoa ei pystytä hakemaan</h1>";
     });
 }
